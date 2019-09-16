9d724ef1029e759bee655c546382dece91dc2068
coderHal <halof.halima@gmail.com>
2019-09-16 16:49:19 +0100

    initial commit

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+const axios = require('axios').default;
+
+axios.get('http://localhost:3000/posts')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
